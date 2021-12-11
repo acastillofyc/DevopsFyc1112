1a75fe42237323be1ee163df86ccfd5747e8c5b3
alexcastillo.pe@gmail.com <alexcastillo.pe@gmail.com>
2021-12-11 11:32:12 -0500

    Agregar archivos de proyecto.

---
+++ DevopsFyc1112.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using DevopsFyc1112;
+using DevopsFyc1112.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace DevopsFyc1112.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+        [TestMethod]
+        public void SumarEnterosTest()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            int resultado = controller.SumarEnteros(4, 9);
+
+            //Assert
+            Assert.AreEqual(13, resultado);
+
+        }
+
+
+    }
+}
---
+++ DevopsFyc1112/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevopsFyc1112.MvcApplication" Language="C#" %>
